1f378d25717b3ee512b55ce6a376c7d4acb044d4
Joshy <josh@joshcruz.net>
2017-01-30 10:53:40 -0800

    can convert a two step equation to a one step equation with intermediate steps

--- src/components/ProblemContainer.jsx
+++ src/components/ProblemContainer.jsx
@@ -35,25 +35,148 @@ class ProblemContainer extends React.Component{
   }
 
   handleSimplify(){
+    var myCurrentProblem = this.state.currentProblem[this.state.currentProblem.length -1];
     var myStateProblemInput = algebra.parse(this.state.currentProblem[this.state.currentProblem.length -1]).toString();
-    var problemListHolder = this.state.currentProblem;
-    problemListHolder.push(myStateProblemInput);
-    this.setState({
-      currentProblem: problemListHolder,
-      hasSimplified: true
-    });
-    console.log("handleSimplify");
-    console.log(this.state.hasSimplified);
-  }
-  render(){
+    var prePush;
+    if(myCurrentProblem === myStateProblemInput){
+      var currentProblemSplit = myCurrentProblem.split(/=/);
+      var currentProblemSideA = currentProblemSplit[0];
+      var currentProblemSideB = currentProblemSplit[1];
 
+      //break up into array elements and count terms for SideA
+      var countSideA = 0
+      var countSideAVariables = 0
+      currentProblemSideA = currentProblemSideA.split(/([-\+\*\/=])/g);
+      //remove empty space created if leading term is negative
+      if(currentProblemSideA[0] === ""){
+        currentProblemSideA.splice(0,1);
+      }
+      for (var i = 0; i < currentProblemSideA.length; i++) {
+        if(!currentProblemSideA[i].match(/[-\+\*\/=]/)){
+          countSideA++;
+        }
+        if(currentProblemSideA[i].match(/[A-Z-a-z]/)){
+          countSideAVariables++;
+        }
+      }
+
+      //break up into array elements and count terms for SideB
+      var countSideB = 0;
+      var countSideBVariables = 0;
+      currentProblemSideB = currentProblemSideB.split(/([-\+\*\/=])/g);
+      //remove empty space created if leading term is negative
+      if(currentProblemSideB[0] === ""){
+        currentProblemSideB.splice(0,1);
+      }
+      for (var i = 0; i < currentProblemSideB.length; i++) {
+        if(!currentProblemSideB[i].match(/[-\+\*\/=]/)){
+          countSideB++;
+        }
+        if(currentProblemSideB[i].match(/[A-Z-a-z]/)){
+          countSideBVariables++;
+        }
+      }
+
+
+      //move term from one side to the other
+      if(countSideAVariables > countSideBVariables && countSideA == countSideB){
+        console.log("move varibales from Side B to Side A");
+        //console.log("countSideAVariables > countSideBVariables && (countSideA-countSideAVariables) > (countSideB - countSideBVariables)");
+        //move variables to sideA
+        //move numbers to sideB
+        var tempVariableNum;
+        for (var i = 0; i < currentProblemSideB.length; i++) {
+          if(currentProblemSideB[i].match(/[A-Za-z]/)){
+            tempVariableNum = i;
+            break;
+          }
+        }
+
+
+        switch (currentProblemSideB[tempVariableNum -1]) {
+
+          case "+":
+          prePush = "-"
+          break;
+          case "-":
+          prePush = "+"
+          break;
+          default:
+          prePush = "-"
+        }
+
+        prePush = prePush + currentProblemSideB[tempVariableNum];
+        currentProblemSideA.push(prePush);
+        currentProblemSideB.push(prePush);
+        currentProblemSideA = currentProblemSideA.toString();
+        currentProblemSideB = currentProblemSideB.toString();
+        myCurrentProblem = currentProblemSideA + "=" + currentProblemSideB;
+        myCurrentProblem = myCurrentProblem.replace(/,/g, '');
+        myCurrentProblem = myCurrentProblem.replace(/ /g, '');
+        var problemListHolder = this.state.currentProblem;
+        problemListHolder.push(myCurrentProblem);
+        console.log("problemListHolder: " + problemListHolder);
+        this.setState({
+          currentProblem: problemListHolder
+        })
+      } else {
+        console.log("countSideAVariables < countSideBVariables-- ELSE");
+        //move numbers to sideB
+        var tempNumTermNum;
+        for (var i = 0; i < currentProblemSideA.length; i++) {
+          if(!currentProblemSideA[i].match(/[A-Za-z-\+\*\/=]/)){
+            tempNumTermNum = i;
+            break;
+          }
+        }
+console.log("tempNumTermNum: " + tempNumTermNum);
+        switch (currentProblemSideA[tempNumTermNum -1]) {
+
+          case "+":
+          prePush = "-"
+          break;
+          case "-":
+          prePush = "+"
+          break;
+          default:
+          prePush = "-"
+        }
+
+        prePush = prePush + currentProblemSideA[tempNumTermNum];
+        console.log("prePush+currentProblemSideA: " + prePush );
+        currentProblemSideA.push(prePush);
+        currentProblemSideB.push(prePush);
+        currentProblemSideA = currentProblemSideA.toString();
+        currentProblemSideB = currentProblemSideB.toString();
+        myCurrentProblem = currentProblemSideA + "=" + currentProblemSideB;
+        myCurrentProblem = myCurrentProblem.replace(/,/g, '');
+        myCurrentProblem = myCurrentProblem.replace(/ /g, '');
+        var problemListHolder = this.state.currentProblem;
+        problemListHolder.push(myCurrentProblem);
+        console.log("problemListHolder: " + problemListHolder);
+        this.setState({
+          currentProblem: problemListHolder
+        })
+
+      }
+
+
+    } else{
+      console.log("Here when can simply simpify");
+      var problemListHolder = this.state.currentProblem;
+      problemListHolder.push(myStateProblemInput);
+      this.setState({
+        currentProblem: problemListHolder,
+        hasSimplified: true
+      });
+    }
+  }
+
+  render(){
     return(
       <div className="panel">
-
-         <ProblemList myProblem={this.state.currentProblem} />
-
-        <button className="btn btn-primary col-sm-6" onClick={this.handleSimplify}>Combine Like Terms (simplify)</button>
-
+        <ProblemList myProblem={this.state.currentProblem} />
+        <button className="btn btn-primary col-sm-6" onClick={this.handleSimplify}>Simplify</button>
       </div>
     )
   }

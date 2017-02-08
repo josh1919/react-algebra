88b4d7de38b5a1897b63b7e3bdc35d57850b1689
Joshy <josh@joshcruz.net>
2017-02-08 12:08:05 -0800

    multi step equation works where final step is divide

--- src/components/ProblemContainer.jsx
+++ src/components/ProblemContainer.jsx
@@ -6,15 +6,6 @@ var Fraction = algebra.Fraction;
 var Expression = algebra.Expression;
 var Equation = algebra.Equation;
 
-
-// var preArray = this.state.newElementString.replace(/ /g, '');
-//TODO check to see there is only one kind of variable
-//will split string at any found operator or equal sign but leave the delimiter
-// var numArray = preArray.split(/([-\+\*\/=])/g);
-// this.setState({exportArray: numArray});
-//  ReactDOM.render(<ProblemRow problem={this.state.exportArray} />, document.getElementById('problem'));
-
-
 class ProblemContainer extends React.Component{
   constructor(props){
     super(props);
@@ -28,17 +19,22 @@ class ProblemContainer extends React.Component{
   componentWillReceiveProps(nextProps){
     if(this.props.myProblem !== nextProps.myProblem){
       this.setState({
-        currentProblem: [nextProps.myProblem], //TODO solution may be here
+        currentProblem: [nextProps.myProblem],
         hasSimplified: false
       });
     }
   }
 
   handleSimplify(){
+    //TODO figure out what to do when I get a division problem
     var myCurrentProblem = this.state.currentProblem[this.state.currentProblem.length -1];
     var myStateProblemInput = algebra.parse(this.state.currentProblem[this.state.currentProblem.length -1]).toString();
     var prePush;
-    if(myCurrentProblem === myStateProblemInput){
+    console.log("*******************************************");
+    console.log("this.state.currentProblem: " + this.state.currentProblem);
+    console.log("myCurrentProblem: " + myCurrentProblem);
+    console.log("myStateProblemInput: " + myStateProblemInput);
+    if(myCurrentProblem == myStateProblemInput){
       var currentProblemSplit = myCurrentProblem.split(/=/);
       var currentProblemSideA = currentProblemSplit[0];
       var currentProblemSideB = currentProblemSplit[1];
@@ -46,16 +42,16 @@ class ProblemContainer extends React.Component{
       //break up into array elements and count terms for SideA
       var countSideA = 0
       var countSideAVariables = 0
-      currentProblemSideA = currentProblemSideA.split(/([-\+\*\/=])/g);
+      currentProblemSideA = currentProblemSideA.split(/([-\+\*\/])/g);
       //remove empty space created if leading term is negative
-      if(currentProblemSideA[0] === ""){
+      if(currentProblemSideA[0] === "" || currentProblemSideA[0] === " "){
         currentProblemSideA.splice(0,1);
       }
       for (var i = 0; i < currentProblemSideA.length; i++) {
-        if(!currentProblemSideA[i].match(/[-\+\*\/=]/)){
+        if(!currentProblemSideA[i].match(/([-\+\*\/=])/)){
           countSideA++;
         }
-        if(currentProblemSideA[i].match(/[A-Z-a-z]/)){
+        if(currentProblemSideA[i].match(/([A-Za-z])/)){
           countSideAVariables++;
         }
       }
@@ -63,27 +59,31 @@ class ProblemContainer extends React.Component{
       //break up into array elements and count terms for SideB
       var countSideB = 0;
       var countSideBVariables = 0;
-      currentProblemSideB = currentProblemSideB.split(/([-\+\*\/=])/g);
+      currentProblemSideB = currentProblemSideB.split(/([-\+\*\/])/g);
       //remove empty space created if leading term is negative
-      if(currentProblemSideB[0] === ""){
+      if(currentProblemSideB[0] === "" || currentProblemSideB[0] === " "){
         currentProblemSideB.splice(0,1);
       }
       for (var i = 0; i < currentProblemSideB.length; i++) {
-        if(!currentProblemSideB[i].match(/[-\+\*\/=]/)){
+        if(!currentProblemSideB[i].match(/([-\+\*\/=])/)){
           countSideB++;
         }
-        if(currentProblemSideB[i].match(/[A-Z-a-z]/)){
+        if(currentProblemSideB[i].match(/([A-Za-z])/)){
           countSideBVariables++;
         }
       }
 
+      console.log("countSideA: " + countSideA);
+      console.log("countSideAVariables: " + countSideAVariables);
+      console.log("countSideB: " + countSideB);
+      console.log("countSideBVariables: " + countSideBVariables);
 
-      //move term from one side to the other
-      if(countSideAVariables > countSideBVariables && countSideA == countSideB){
-        console.log("move varibales from Side B to Side A");
+      //move term from one side to the other // Moves variables to sideA
+      if(countSideBVariables > 0){
+        console.log("move variables from Side B to Side A");
         //console.log("countSideAVariables > countSideBVariables && (countSideA-countSideAVariables) > (countSideB - countSideBVariables)");
         //move variables to sideA
-        //move numbers to sideB
+
         var tempVariableNum;
         for (var i = 0; i < currentProblemSideB.length; i++) {
           if(currentProblemSideB[i].match(/[A-Za-z]/)){
@@ -119,17 +119,18 @@ class ProblemContainer extends React.Component{
         this.setState({
           currentProblem: problemListHolder
         })
-      } else {
-        console.log("countSideAVariables < countSideBVariables-- ELSE");
+      } else if(countSideA > countSideAVariables) { //TODO fix this so that if the number are on sideA
         //move numbers to sideB
+        console.log("countSideA - countSideAVariables > countSideA - countSideBVariables ELSE IF");
         var tempNumTermNum;
+        console.log("currentProblemSideA: " + currentProblemSideA);
         for (var i = 0; i < currentProblemSideA.length; i++) {
-          if(!currentProblemSideA[i].match(/[A-Za-z-\+\*\/=]/)){
+          if(!currentProblemSideA[i].match(/[A-Za-z\+\*\/=-]/)){
             tempNumTermNum = i;
             break;
           }
         }
-console.log("tempNumTermNum: " + tempNumTermNum);
+
         switch (currentProblemSideA[tempNumTermNum -1]) {
 
           case "+":
@@ -158,11 +159,42 @@ console.log("tempNumTermNum: " + tempNumTermNum);
           currentProblem: problemListHolder
         })
 
+      } else if(countSideA == countSideB && countSideA == 1){
+        console.log("should be division");
+        console.log("currentProblemSideA[0]: " + currentProblemSideA[0]);
+        //TODO fix these next lines. they are wrong because they assume they are recieving a string [actually receives an array]. I need to fix them to prepare for the Simplification
+        // when I go to
+         var temp;
+        // var divider = currentProblemSideA.match(/\d/g);
+        // currentProblemSideA = currentProblemSideA + "/" + divider;
+
+        if(currentProblemSideA[0] == "-"){
+          temp = "/(-" + currentProblemSideA[1].match(/\d/g) + ")"
+        } else {
+          temp = "/(" + currentProblemSideA[0].match(/\d/g) + ")";
+        }
+
+        //prePush = prePush + currentProblemSideA[tempNumTermNum];
+        console.log("temp before pushed to array: " + temp );
+        currentProblemSideA.push(temp);
+        currentProblemSideB.push(temp);
+        currentProblemSideA = currentProblemSideA.toString();
+        currentProblemSideB = currentProblemSideB.toString();
+        myCurrentProblem = currentProblemSideA + "=" + currentProblemSideB;
+        myCurrentProblem = myCurrentProblem.replace(/,/g, '');
+        myCurrentProblem = myCurrentProblem.replace(/ /g, '');
+        var problemListHolder = this.state.currentProblem;
+        problemListHolder.push(myCurrentProblem);
+        console.log("problemListHolder: " + problemListHolder);
+        this.setState({
+          currentProblem: problemListHolder
+        })
+
       }
 
 
-    } else{
-      console.log("Here when can simply simpify");
+    } else {
+      console.log("Simplification");
       var problemListHolder = this.state.currentProblem;
       problemListHolder.push(myStateProblemInput);
       this.setState({

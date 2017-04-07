d510a279abd5f66cfd38890122dc9b494638c918
Joshy <josh@joshcruz.net>
2017-04-07 09:07:40 -0700

    Change input area and submit button to regular text(no longer able to input) and reset button once submitted

--- src/components/EnterProblem.jsx
+++ src/components/EnterProblem.jsx
@@ -28,43 +28,47 @@ class EnterProblem extends React.Component{
     e.preventDefault();
     //removes all white space in inputed problem
     var preArray = this.state.currentInput.replace(/ /g, '').toString();
-      //Input validation
-      if(!preArray.match(/[a-z][a-z]|[A-Z][A-Z]|[A-Z][a-z]|[a-z][A-Z]/g)){//if it does not contain two variables in a row continue
+    //Input validation
+    if(!preArray.match(/[a-z][a-z]|[A-Z][A-Z]|[A-Z][a-z]|[a-z][A-Z]/g)){//if it does not contain two variables in a row continue
 
-          this.setState({
-            currentInput: preArray,
-            stepList: "Initial Problem",
-            submitted: true
-          });
+      this.setState({
+        currentInput: preArray,
+        stepList: "Initial Problem",
+        submitted: true
+      });
 
-      } else {//if input invalid
-        this.setState({
-          submitted: false
-        })
-        alert("input invalid, try something else");
-      }
-    };
+    } else {//if input invalid
+      this.setState({
+        submitted: false
+      })
+      alert("input invalid, try something else");
+    }
+  };
 
   render(){
-    let divStyle={
-      margin:10,
-    };
+    let divStyle={margin:10,};
 
     return (
 
       <div className='row' style={divStyle}>
-        <form onSubmit={this.onSubmit.bind(this)}>
-          <div className='input-group'>
+        {!this.state.submitted ?
+          <form onSubmit={this.onSubmit.bind(this)}>
+            <div className='input-group'>
 
-            <input className='form-control' onChange={this.onChange.bind(this)} value={this.state.currentInput}/>
-            <span className='input-group-btn'>
-              <button className='btn btn-primary' >Submit</button>
-            </span>
+              <input className='form-control' onChange={this.onChange.bind(this)} value={this.state.currentInput}/>
+              <span className='input-group-btn'>
+                <button className='btn btn-primary'>Submit</button>
+              </span>
+            </div>
+          </form>
+          :
+          <div>
+            <div className="col-xs-9 text-right">{this.state.currentInput}</div>
+            <button className="btn" onClick={this.onReset}>Reset</button>
           </div>
-        </form>
-
+        }
         {this.state.submitted ? <ProblemContainer myProblem={this.state.currentInput} stepList={this.state.stepList} /> : null}
-        <button className="btn col-xs-12" onClick={this.onReset}>Reset</button>
+
       </div>
 
     )
